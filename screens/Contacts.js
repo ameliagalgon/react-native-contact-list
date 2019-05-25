4fc73d71da02b8c15db95cc2863cd830ba3668b0
Amelia Galgon <agalgon@gmail.com>
2019-05-24 20:44:40 -0400

    react navigation for contacts and profile

---
+++ screens/Contacts.js
@@ -0,0 +1,83 @@
+import * as React from 'react';
+import {
+    StyleSheet,
+    Text,
+    View,
+    FlatList,
+    ActivityIndicator,
+} from 'react-native';
+
+import ContactListItem from '../components/ContactListItem';
+
+import { fetchContacts } from "../utils/api";
+
+const keyExtractor = ({ phone }) => phone;
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: 'white',
+        justifyContent: 'center',
+        flex: 1
+    }
+});
+
+export default class Contacts extends React.Component {
+    state = {
+        contacts: [],
+        loading: true,
+        error: false,
+    };
+
+    async componentDidMount() {
+        try {
+            const contacts = await fetchContacts();
+
+            this.setState({
+                contacts,
+                loading: false,
+                error: false,
+            });
+        } catch (e) {
+            this.setState({
+                loading: false,
+                error: true
+            });
+        }
+    }
+
+    renderContact = ({ item }) => {
+        const { navigation: { navigate } } = this.props;
+        const { id, name, avatar, phone } = item;
+        
+        return (
+            <ContactListItem
+                avatar={avatar}
+                phone={phone}
+                name={name}
+                onPress={() => navigate('Profile', { contact: item })}
+            />
+        );
+    }
+
+    render() {
+        const { loading, contacts, error } = this.state;
+
+        const contactsSorted = contacts.sort((a, b) => {
+            a.name.localeCompare(b.name)
+        });
+
+        return (
+            <View style={styles.container}>
+                { loading && <ActivityIndicator size={'large'}/> }
+                { error && <Text>Error...</Text> }
+                { !loading && !error &&
+                    <FlatList
+                        data={contactsSorted}
+                        keyExtractor={keyExtractor}
+                        renderItem={this.renderContact}
+                    />
+                }
+            </View>
+        );
+    }
+}
